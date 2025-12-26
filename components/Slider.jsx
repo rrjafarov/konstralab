9f1de15a8d5a9bc2103a3a09e9c93190a85ee32e
rrjafarov <rafael.cefer.4050@gmail.com>
2025-12-26 12:04:45 +0400

    Add slider styles and refactor CareersPageSlider component

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,113 +1,3 @@
-// "use client";
-// import Image from "next/image";
-// import React from "react";
-// import Link from "next/link";
-// import { Fancybox } from "@fancyapps/ui";
-// import "@fancyapps/ui/dist/fancybox/fancybox.css";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import "swiper/css/autoplay";
-// import "../app/[locale]/globals.scss";
-// import { Autoplay, Navigation } from "swiper/modules";
-
-// Fancybox.bind("[data-fancybox]", {
-//   dragToClose: false,
-//   Image: {
-//     zoom: false,
-//   },
-// });
-
-// const CareersPageSlider = ({ gallery,t }) => {
-//   const slidesData = gallery?.data?.data || [];
-
-//   return (
-//     <>
-//       <div className="sliderSectionTitle">
-//         <span>
-//           {t?.galleryTitle || "Discover the art of precision in construction"}
-//         </span>
-//       </div>
-//       <Swiper
-//         slidesPerView={5}
-//         spaceBetween={-5}
-//         speed={2000}
-//         loop={true}
-//         centeredSlides={true}
-//         modules={[Navigation, Autoplay]}
-//         navigation={{
-//           nextEl: ".careersCustom-next",
-//           prevEl: ".careersCustom-prev",
-//         }}
-//         breakpoints={{
-//           320: { slidesPerView: 2 },
-//           480: { slidesPerView: 2 },
-//           640: { slidesPerView: 3 },
-//           767: { slidesPerView: 3 },
-//           1024: { slidesPerView: 5 },
-//           1280: { slidesPerView: 5 },
-//         }}
-//         className="mySwiper careersSwiper"
-//       >
-//         {slidesData.map((item) =>
-//           item.gallery.map((imgUrl, idx) => (
-//             <SwiperSlide key={`${item.id}-${idx}`}>
-//               <Link
-//                 href={`https://admin-konstralab.onestudio.az/storage${imgUrl}`}
-//                 className="careersSliderGalleryImg"
-//                 data-fancybox="videos"
-//               >
-//                 <Image
-//                   src={`https://admin-konstralab.onestudio.az/storage${imgUrl}`}
-//                   className="careersSliderCardImg"
-//                   alt={`slide-${item.id}-${idx}`}
-//                   width={400}
-//                   height={400}
-//                 />
-//               </Link>
-//             </SwiperSlide>
-//           ))
-//         )}
-//       </Swiper>
-
-//       <div className="careersCustom-navigation">
-//         <button className="careersCustom-prev">
-//           <img src="/icon/careersLeftArrow.svg" alt="" />
-//         </button>
-//         <button className="careersCustom-next">
-//           <img src="/icon/careersRightArrow.svg" alt="" />
-//         </button>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default CareersPageSlider;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 "use client";
 import Image from "next/image";
 import React from "react";
@@ -134,9 +24,9 @@ const CareersPageSlider = ({ gallery,t }) => {
   return (
     <>
       <div className="sliderSectionTitle">
-        <h2>
+        <span>
           {t?.galleryTitle || "Discover the art of precision in construction"}
-        </h2>
+        </span>
       </div>
       <Swiper
         slidesPerView={5}
@@ -158,11 +48,10 @@ const CareersPageSlider = ({ gallery,t }) => {
           1280: { slidesPerView: 5 },
         }}
         className="mySwiper careersSwiper"
-        style={{ overflow: 'visible' }}
       >
         {slidesData.map((item) =>
           item.gallery.map((imgUrl, idx) => (
-            <SwiperSlide key={`${item.id}-${idx}`} style={{ padding: '0 15px' }}>
+            <SwiperSlide key={`${item.id}-${idx}`}>
               <Link
                 href={`https://admin-konstralab.onestudio.az/storage${imgUrl}`}
                 className="careersSliderGalleryImg"
@@ -194,3 +83,114 @@ const CareersPageSlider = ({ gallery,t }) => {
 };
 
 export default CareersPageSlider;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "use client";
+// import Image from "next/image";
+// import React from "react";
+// import Link from "next/link";
+// import { Fancybox } from "@fancyapps/ui";
+// import "@fancyapps/ui/dist/fancybox/fancybox.css";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/autoplay";
+// import "../app/[locale]/globals.scss";
+// import { Autoplay, Navigation } from "swiper/modules";
+
+// Fancybox.bind("[data-fancybox]", {
+//   dragToClose: false,
+//   Image: {
+//     zoom: false,
+//   },
+// });
+
+// const CareersPageSlider = ({ gallery,t }) => {
+//   const slidesData = gallery?.data?.data || [];
+
+//   return (
+//     <>
+//       <div className="sliderSectionTitle">
+//         <h2>
+//           {t?.galleryTitle || "Discover the art of precision in construction"}
+//         </h2>
+//       </div>
+//       <Swiper
+//         slidesPerView={5}
+//         spaceBetween={-5}
+//         speed={2000}
+//         loop={true}
+//         centeredSlides={true}
+//         modules={[Navigation, Autoplay]}
+//         navigation={{
+//           nextEl: ".careersCustom-next",
+//           prevEl: ".careersCustom-prev",
+//         }}
+//         breakpoints={{
+//           320: { slidesPerView: 2 },
+//           480: { slidesPerView: 2 },
+//           640: { slidesPerView: 3 },
+//           767: { slidesPerView: 3 },
+//           1024: { slidesPerView: 5 },
+//           1280: { slidesPerView: 5 },
+//         }}
+//         className="mySwiper careersSwiper"
+//         style={{ overflow: 'visible' }}
+//       >
+//         {slidesData.map((item) =>
+//           item.gallery.map((imgUrl, idx) => (
+//             <SwiperSlide key={`${item.id}-${idx}`} style={{ padding: '0 15px' }}>
+//               <Link
+//                 href={`https://admin-konstralab.onestudio.az/storage${imgUrl}`}
+//                 className="careersSliderGalleryImg"
+//                 data-fancybox="videos"
+//               >
+//                 <Image
+//                   src={`https://admin-konstralab.onestudio.az/storage${imgUrl}`}
+//                   className="careersSliderCardImg"
+//                   alt={`slide-${item.id}-${idx}`}
+//                   width={400}
+//                   height={400}
+//                 />
+//               </Link>
+//             </SwiperSlide>
+//           ))
+//         )}
+//       </Swiper>
+
+//       <div className="careersCustom-navigation">
+//         <button className="careersCustom-prev">
+//           <img src="/icon/careersLeftArrow.svg" alt="" />
+//         </button>
+//         <button className="careersCustom-next">
+//           <img src="/icon/careersRightArrow.svg" alt="" />
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default CareersPageSlider;
